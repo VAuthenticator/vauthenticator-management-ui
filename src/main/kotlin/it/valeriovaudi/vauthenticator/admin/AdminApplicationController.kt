b53956cacf5e86726f56646029ca00847eb6df38
mrflick72 <valerio.vaudi@gmail.com>
2022-05-26 22:49:28 +0200

    session management issue fix

--- src/main/kotlin/it/valeriovaudi/vauthenticator/admin/AdminApplicationController.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/admin/AdminApplicationController.kt
@@ -6,10 +6,14 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class AdminApplicationController(@Value("\${vauthenticator.host}") private val vauthenticatorHost : String) {
+class AdminApplicationController(
+        @Value("\${vauthenticator.session-management.rp-iframe.host}") private val managementUiHost : String,
+        @Value("\${vauthenticator.host}") private val vauthenticatorHost : String
+        ) {
 
     @GetMapping("/secure/admin/index")
     fun view(model : Model) : String {
+        model.addAttribute("rpSessionManagementIFrame", "$managementUiHost/session/management")
         model.addAttribute("opSessionManagementIFrame", "$vauthenticatorHost/session/management")
         return "secure/admin"
     }
