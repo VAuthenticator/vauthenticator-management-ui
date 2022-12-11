8ee6ad55f9fd21e722f96312b086461bde277222
mrflick72 <valerio.vaudi@gmail.com>
2022-12-11 16:17:41 +0100

    [skip ci] WIP

--- src/main/kotlin/it/valeriovaudi/vauthenticator/config/WebSecurityConfig.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/config/WebSecurityConfig.kt
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService
-import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.client.RestTemplate
 
