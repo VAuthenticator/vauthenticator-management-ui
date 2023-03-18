7cd362783bf93910a2b35841119045e7fa2596c2
mrflick72 <valerio.vaudi@gmail.com>
2023-03-18 21:49:15 +0100

    load asset on VAuthenticator SAAS

--- src/main/kotlin/com/vauthenticator/management/web/StaticController.kt
+++ src/main/kotlin/com/vauthenticator/management/web/StaticController.kt
@@ -1,6 +1,7 @@
 package com.vauthenticator.management.web
 
 import com.vauthenticator.management.document.Document
+import com.vauthenticator.management.document.DocumentRepository
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.cache.caffeine.CaffeineCache
 import org.springframework.http.ResponseEntity
@@ -10,11 +11,16 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @ConditionalOnProperty("asset-server.on-s3.enabled", havingValue = "true", matchIfMissing = true)
-class StaticController(private val staticContentLocalCache: CaffeineCache) {
+class StaticController(
+    private val documentRepository: DocumentRepository,
+    private val staticContentLocalCache: CaffeineCache
+) {
 
     @GetMapping("/static/content/asset/{assetName}")
     fun assetContent(@PathVariable assetName: String): ResponseEntity<*> {
-        val document = staticContentLocalCache.get(assetName, Document::class.java)!!
+        staticContentLocalCache.get(assetName, Document::class.java)!!
+        val document = documentRepository.loadDocument("static-management-ui", "content/asset/$assetName")
+
         return ResponseEntity.ok()
             .header("Content-Type", document.contentType)
             .body(document.content)
