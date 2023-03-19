dd51876b45ab4bad0e7b8ddd3954ad985f22495f
mrflick72 <valerio.vaudi@gmail.com>
2023-03-19 20:08:38 +0100

    load asset on VAuthenticator SAAS

--- src/main/kotlin/com/vauthenticator/management/web/StaticController.kt
+++ src/main/kotlin/com/vauthenticator/management/web/StaticController.kt
@@ -1,8 +1,9 @@
 package com.vauthenticator.management.web
 
-import com.vauthenticator.management.document.DocumentRepository
+import com.vauthenticator.management.document.Document
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.cache.caffeine.CaffeineCache
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -10,24 +11,13 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @ConditionalOnProperty("asset-server.on-s3.enabled", havingValue = "true", matchIfMissing = true)
-class StaticController(
-    private val documentRepository: DocumentRepository
-//    , private val staticContentLocalCache: CaffeineCache
-) {
+class StaticController(private val staticContentLocalCache: CaffeineCache) {
 
     private val logger = LoggerFactory.getLogger(StaticController::class.java)
 
     @GetMapping("/static/content/asset/{assetName}")
     fun assetContent(@PathVariable assetName: String): ResponseEntity<*> {
-        /*
-            try {
-             logger.info("assetName : $assetName")
-             staticContentLocalCache.get(assetName, Document::class.java)!!
-             }catch (e:Exception){
-                 e.printStackTrace()
-             }
-         */
-        val document = documentRepository.loadDocument("static-management-ui", "content/asset/$assetName")
+        val document = staticContentLocalCache.get(assetName, Document::class.java)!!
 
         return ResponseEntity.ok()
             .header("Content-Type", document.contentType)
