0d658bd46f4a6d45c9988540c0b3d9c3d1c0a254
mrflick72 <valerio.vaudi@gmail.com>
2023-04-12 00:49:23 +0200

    empty page for upload mail template

---
+++ src/main/frontend/app/admin/templates/MailTemplatePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AdminTemplate from "../../component/AdminTemplate";
+
+
+const MailTemplatePage = () => {
+    const pageTitle = "Mail Template Management"
+
+    return (
+        <AdminTemplate maxWidth="xl"  page={pageTitle}>
+
+
+        </AdminTemplate>
+    )
+}
+
+export default MailTemplatePage
