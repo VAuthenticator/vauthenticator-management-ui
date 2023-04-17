1548b4ce552bccc10dbdaa912b0604618c21e3b9
mrflick72 <valerio.vaudi@gmail.com>
2023-04-17 23:01:02 +0200

    success/failure feedback on same mail template

--- src/main/frontend/app/admin/templates/MailTemplatePage.js
+++ src/main/frontend/app/admin/templates/MailTemplatePage.js
@@ -1,7 +1,7 @@
 import React, {useRef, useState} from 'react';
 import AdminTemplate from "../../component/AdminTemplate";
 import vauthenticatorStyles from "../../theme/styles";
-import {useTheme} from "@mui/material";
+import {Alert, Snackbar, useTheme} from "@mui/material";
 import Typography from "@material-ui/core/Typography";
 
 import Separator from "../../component/Separator";
@@ -16,17 +16,43 @@ const MailTemplatePage = () => {
 
     let mailContent = useRef()
     const [mailType, setMailType] = useState("")
+    const [openSucceeded, setOpenSucceeded] = React.useState(false);
+    const [openFailure, setOpenFailure] = React.useState(false);
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenSucceeded(false);
+        setOpenFailure(false);
+    };
+
     let saveMailTemplate = () => {
         saveMailTemplateFor({
             mailType: mailType.value,
             body: mailContent.current.innerText
         })
-            .then(_ => {
+            .then(r => {
+                if(r.status == 204){
+                    setOpenSucceeded(true);
+                } else {
+                    setOpenFailure(false);
+                }
             })
     };
     return (
         <AdminTemplate maxWidth="xl" page={pageTitle}>
-
+            <Snackbar open={openSucceeded} autoHideDuration={6000}>
+                <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                    Mail Template save succeeded
+                </Alert>
+            </Snackbar>
+            <Snackbar open={openFailure} autoHideDuration={6000}>
+                <Alert onClose={handleClose} severity="failure" sx={{ width: '100%' }}>
+                    Mail Template save failed
+                </Alert>
+            </Snackbar>
             <Typography variant="h2">
                 Mail Template
             </Typography>
