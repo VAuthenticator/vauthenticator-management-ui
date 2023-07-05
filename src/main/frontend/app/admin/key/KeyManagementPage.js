b0239c09dfc2ea207a8b1280aa92af07f5c586e7
mrflick72 <valerio.vaudi@gmail.com>
2023-07-05 21:31:24 +0200

    upgrade

--- src/main/frontend/app/admin/key/KeyManagementPage.js
+++ src/main/frontend/app/admin/key/KeyManagementPage.js
@@ -3,6 +3,7 @@ import AdminTemplate from "../../component/AdminTemplate";
 import {Delete} from "@material-ui/icons";
 import {deleteKeyFor, findAllKeys} from "./KeyRepository";
 import StickyHeadTable from "../../component/StickyHeadTable";
+import ConfirmationDialog from "../../component/ConfirmationDialog";
 
 const columns = [
     {id: 'masterKey', label: 'Maser Key', minWidth: 170},
@@ -13,14 +14,25 @@ const columns = [
 const KeysManagementPage = () => {
     const pageTitle = "Keys Management"
     const [keys, setKeys] = React.useState([])
+    const [openConfirmationDeleteKeyDialog, setOpenConfirmationDeleteKeyDialog] = React.useState(false)
+    const handleCloseConfirmationDialog = (refresh) => {
+        setOpenConfirmationDeleteKeyDialog(false);
+        if (refresh) {
+            fetchAllKeys();
+        }
+    };
+
+    const deleteKey = (kid) => deleteKeyFor(kid)
+        .then(response => {
+            if (response.status === 204) {
+                fetchAllKeys()
+            }
+        })
 
     const getDeleteLinkFor = (kid) => {
         return <Delete onClick={() => {
-            deleteKeyFor(kid)
-                .then(response => {
-                    if (response.status === 204) {
-                        fetchAllKeys()
-                    }
+            deleteKey(kid)
+                .then(_ => {
                 })
         }}/>;
     }
@@ -52,6 +64,12 @@ const KeysManagementPage = () => {
     return (
         <AdminTemplate maxWidth="xl" page={pageTitle}>
             <StickyHeadTable columns={columns} rows={keys}/>
+            <ConfirmationDialog maxWidth="md"
+                                open={openConfirmationDeleteKeyDialog}
+                                onExecute={deleteKey}
+                                onClose={handleCloseConfirmationDialog}
+                                message="Are you sure delete the selected key"
+                                title="Key delete"/>
         </AdminTemplate>
     )
 }
