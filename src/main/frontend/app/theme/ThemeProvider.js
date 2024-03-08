f084fccb16dce084a8e0c8a760eb2f621e420792
mrflick72 <valerio.vaudi@gmail.com>
2024-03-08 23:03:01 +0100

    - styling fix

--- src/main/frontend/app/theme/ThemeProvider.js
+++ src/main/frontend/app/theme/ThemeProvider.js
@@ -1,16 +1,6 @@
 import {createTheme} from "@mui/material";
 
-const padding = "10px"
 const themeProvider = createTheme({
-    formInputText: {
-        padding: padding
-    },
-    formButton: {
-        padding: padding
-    },
-    formDatePicker: {
-        padding: padding
-    },
     palette: {
         primary: {
             main: '#252624',
@@ -21,6 +11,15 @@ const themeProvider = createTheme({
             contrastText: '#fff',
         },
     },
+    components: {
+        MuiGrid: {
+            styleOverrides: {
+                root: {
+                    padding: "10px"
+                }
+            }
+        }
+    }
 });
 
 export default themeProvider
