35769af95570634bcd9e931a91356d4f22f94325
mrflick72 <valerio.vaudi@gmail.com>
2024-04-19 19:50:52 +0200

    [skip ci] Home Page tsx migration

--- src/main/frontend/app/component/MenuCardContainer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Grid} from "@mui/material";
-
-export default function MenuCardContainer({children}) {
-    return <Grid container spacing={2}>
-        {children}
-    </Grid>
-}
